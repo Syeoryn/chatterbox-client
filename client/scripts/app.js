3bac071d13bf135c4ffd973b0161a1e119bc0de8
Hack Reactor Students <accounts@hackreactor.com>
2014-04-29 11:47:48 -0700

    Adds app.send.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,24 @@
-// YOUR CODE HERE:
+var app = {};
 
+app.init = function(){
+
+};
+
+app.send = function(messageObject){
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    type: 'POST',
+    data: JSON.stringify(messageObject),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      console.error('chatterbox: Failed to send message');
+    }
+  });
+};
+
+app.fetch = function(){
+
+};
